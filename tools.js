f2464c168cb02bdd484c27562177ebc27c425dce
mattlee46 <matt@picchu.io>
2023-02-06 00:27:15 +0800

    UI update

--- tools.js
+++ tools.js
@@ -15,95 +15,99 @@ var slackId;
 // 31-60 - Level3
 // 61+ - Level4
 let data_pfp =[
-	["0x0042e5Dc4E5bE09cfaf5722dd77cFa92D1AF7341",3,4,5,6,4],
-	["0x1f6c07a5a25faf5c8315826827c15a8340f30767",3,4,5,2,2],
-	["0x37d3745b398a4dcd2daf62a54d507d353079c72a",3,6,1,2,4],
-	["0x4ec5b2606ac7d20b1b0030d156f6d789b5873abd",3,5,6,2,3],
-	["0x5235a7fa85e97d9cb2f2e57af97c3945ee5edc01",3,6,1,4,6],
-	["0x58a028C88D61D8cFAA324DCCC13e7E5CbB33cF6E",3,1,1,4,3],
-	["0x5d3f7C09c0132C2c5A78ec2E654b9068f5550954",3,2,2,1,2],
-	["0x77f946376719ef066028a2064cdf78df857d16b4",3,6,6,4,3],
-	["0x7ea1eaa27b313d04d359bf3e654fe927376e31bb",3,5,6,4,2],
-	["0x7fF3c36212F5B10846B7A2569b7b8a822a900050",3,1,6,4,2],
-	["0x9b20439B290C3FB24B40AFF366cD3fdA3B8bC0dD",3,2,6,5,6],
-	["0xA471495C9a27Dc34910397cD2f2d85149ded8b9B",3,2,1,1,1],
-	["0xb62409c9cd6f9d2ab162901552590b76438f35c0",3,6,4,2,2],
-	["0xbeb01aab5167402a5e89d057a714fabff94c3112",3,6,1,1,4],
-	["0xbfc14bc72590eb653af0873d3401fac602942ad3",3,4,3,3,4],
-	["0xC40dd2e85459e841021DD84F550645901aA337F9",3,5,6,4,1],
-	["0xc5a6d80bc7a2c3093a68e2ad05e8205dc0c0726b",3,5,1,6,5],
-	["0xCC14dB67F6E8d7CF0D9885223A3099A75D7Bdff2",3,4,4,6,2],
-	["0xcd0aa623f14cd369c1a2d3aacb96cf9157323795",3,2,2,6,2],
-	["0xcfb9fc585ed11bb8a6e79547920d825b6666bf55",3,6,2,6,4],
-	["0xdE1391114b3FcC3053c25127B399080F89CF6E71",3,6,5,5,1],
-	["0x0795632dbe95bde7c896d48f794278661ef2054c",4,4,2,3,3],
-	["0x3c0b9c1690A94Fcc6994402d1ed8754439941835",4,3,1,5,6],
-	["0xaFCcA66cA8737FE647B7E94CB1a0891a72ab3AE5",5,6,3,3,3],
-	["0x4f89dF4406224693db4FD496A4066c8dE240CC53",6,2,4,4,4],
-	["0x2fbaf345102ad37f2f4dbc1017089eed978b3c05",10,3,2,1,3],
-	["0x0624Aa196299088f7229dEFcee316dF5198C1255",12,6,3,2,2],
-	["0xB58dae4ebFCa3AeB21fF534991367f744b34c118",13,2,6,5,4],
-	["0xb7fcae88cab094a3372ce59b15be4c6e5f408762",13,5,2,5,5],
-	["0xd1444046e3987e44c8757a2400e9f8b7539f86b6",14,5,2,2,2],
-	["0x1cca19b823afa773b09708d94d2ee6ff96c60057",20,4,4,6,5],
-	["0x59F3c40a980b4155C8ED22DA76B4c8Ba72020291",20,3,6,4,6],
-	["0x01Bf77d8adaFd930b99C4fc01Ed5eAF616Bc6D93",23,5,4,6,1],
-	["0x7CD7D564c2BfA56A745E89204C457E1205595348",25,6,5,2,4],
-	["0xf146f3bf137683c6d2fe266f6a2dc352a615eb0c",25,6,1,4,4],
-	["0x7d5b4d1da35bb09c950295d6064a0f167346279f",26,2,1,3,4],
-	["0xdeE9D3a767e08AFCffc909e55d853a0069c2f46F",27,5,3,1,2],
-	["0x93420FFA0D098a0f10E34cd4495500a1CAf35888",31,6,5,4,3],
-	["0xab51ad23d222fd0afb4e29f3244402af9aa3c420",33,5,2,1,6],
-	["0xfBfaC05bd50e868fBAa842363883b23d9045A24f",37,3,2,4,4],
-	["0xcf61e54644f634d22b751b586f1fbe5f8f562c60",44,1,1,1,6],
-	["0xc3e06A42655E1988476089773C6f0ACB697167E7",45,5,6,6,5],
-	["0x15c7281842A45465B4cbb8F89111d99e36e5bab8",105,6,3,4,4]
+["0x0042e5dc4e5be09cfaf5722dd77cfa92d1af7341",3,4,5,6,4],
+["0x1f6c07a5a25faf5c8315826827c15a8340f30767",3,4,5,2,2],
+["0x4ec5b2606ac7d20b1b0030d156f6d789b5873abd",3,5,6,2,3],
+["0x5235a7fa85e97d9cb2f2e57af97c3945ee5edc01",3,6,1,4,6],
+["0x58a028c88d61d8cfaa324dccc13e7e5cbb33cf6e",3,1,1,4,3],
+["0x5d3f7c09c0132c2c5a78ec2e654b9068f5550954",3,2,2,1,2],
+["0x77f946376719ef066028a2064cdf78df857d16b4",3,6,6,4,3],
+["0x7ea1eaa27b313d04d359bf3e654fe927376e31bb",3,5,6,4,2],
+["0x7ff3c36212f5b10846b7a2569b7b8a822a900050",3,1,6,4,2],
+["0x9b20439b290c3fb24b40aff366cd3fda3b8bc0dd",3,2,6,5,6],
+["0xa471495c9a27dc34910397cd2f2d85149ded8b9b",3,2,1,1,1],
+["0xb62409c9cd6f9d2ab162901552590b76438f35c0",3,6,4,2,2],
+["0xbeb01aab5167402a5e89d057a714fabff94c3112",3,6,1,1,4],
+["0xbfc14bc72590eb653af0873d3401fac602942ad3",3,4,3,3,4],
+["0xc40dd2e85459e841021dd84f550645901aa337f9",3,5,6,4,1],
+["0xc5a6d80bc7a2c3093a68e2ad05e8205dc0c0726b",3,5,1,6,5],
+["0xcc14db67f6e8d7cf0d9885223a3099a75d7bdff2",3,4,4,6,2],
+["0xcd0aa623f14cd369c1a2d3aacb96cf9157323795",3,2,2,6,2],
+["0xcfb9fc585ed11bb8a6e79547920d825b6666bf55",3,6,2,6,4],
+["0xde1391114b3fcc3053c25127b399080f89cf6e71",3,6,5,5,1],
+["0x0795632dbe95bde7c896d48f794278661ef2054c",4,4,2,3,3],
+["0xafcca66ca8737fe647b7e94cb1a0891a72ab3ae5",5,6,3,3,3],
+["0xf472067df9f1ef13da784f9181205506bc2cf01a",5,3,2,2,6],
+["0x4f89df4406224693db4fd496a4066c8de240cc53",6,2,4,4,4],
+["0x2fbaf345102ad37f2f4dbc1017089eed978b3c05",10,3,2,1,3],
+["0x37d3745b398a4dcd2daf62a54d507d353079c72a",11,6,1,2,4],
+["0xb7fcae88cab094a3372ce59b15be4c6e5f408762",13,5,2,5,5],
+["0x01bf77d8adafd930b99c4fc01ed5eaf616bc6d93",23,5,4,6,1],
+["0x3c0b9c1690a94fcc6994402d1ed8754439941835",24,3,1,5,6],
+["0xd1444046e3987e44c8757a2400e9f8b7539f86b6",26,5,2,2,2],
+["0x7d5b4d1da35bb09c950295d6064a0f167346279f",26,2,1,3,4],
+["0xb58dae4ebfca3aeb21ff534991367f744b34c118",28,2,6,5,4],
+["0x4744cda32be7b3e75b9334001da9ed21789d4c0d",28,3,1,4,2],
+["0x59f3c40a980b4155c8ed22da76b4c8ba72020291",30,3,6,4,6],
+["0xf146f3bf137683c6d2fe266f6a2dc352a615eb0c",31,6,1,4,4],
+["0x1cca19b823afa773b09708d94d2ee6ff96c60057",35,4,4,6,5],
+["0x7cd7d564c2bfa56a745e89204c457e1205595348",37,6,5,2,4],
+["0xdee9d3a767e08afcffc909e55d853a0069c2f46f",43,5,3,1,2],
+["0x0624aa196299088f7229defcee316df5198c1255",44,6,3,2,2],
+["0xcf61e54644f634d22b751b586f1fbe5f8f562c60",44,1,1,1,6],
+["0x93420ffa0d098a0f10e34cd4495500a1caf35888",51,6,5,4,3],
+["0xfbfac05bd50e868fbaa842363883b23d9045a24f",52,3,2,4,4],
+["0xab51ad23d222fd0afb4e29f3244402af9aa3c420",64,5,2,1,6],
+["0xc3e06a42655e1988476089773c6f0acb697167e7",82,5,6,6,5],
+["0x15c7281842a45465b4cbb8f89111d99e36e5bab8",163,6,3,4,4]
 ];
 
 let data_address = [
-	"0x0042e5Dc4E5bE09cfaf5722dd77cFa92D1AF7341",
+	"0x0042e5dc4e5be09cfaf5722dd77cfa92d1af7341",
 	"0x1f6c07a5a25faf5c8315826827c15a8340f30767",
-	"0x37d3745b398a4dcd2daf62a54d507d353079c72a",
 	"0x4ec5b2606ac7d20b1b0030d156f6d789b5873abd",
 	"0x5235a7fa85e97d9cb2f2e57af97c3945ee5edc01",
-	"0x58a028C88D61D8cFAA324DCCC13e7E5CbB33cF6E",
-	"0x5d3f7C09c0132C2c5A78ec2E654b9068f5550954",
+	"0x58a028c88d61d8cfaa324dccc13e7e5cbb33cf6e",
+	"0x5d3f7c09c0132c2c5a78ec2e654b9068f5550954",
 	"0x77f946376719ef066028a2064cdf78df857d16b4",
 	"0x7ea1eaa27b313d04d359bf3e654fe927376e31bb",
-	"0x7fF3c36212F5B10846B7A2569b7b8a822a900050",
-	"0x9b20439B290C3FB24B40AFF366cD3fdA3B8bC0dD",
-	"0xA471495C9a27Dc34910397cD2f2d85149ded8b9B",
+	"0x7ff3c36212f5b10846b7a2569b7b8a822a900050",
+	"0x9b20439b290c3fb24b40aff366cd3fda3b8bc0dd",
+	"0xa471495c9a27dc34910397cd2f2d85149ded8b9b",
 	"0xb62409c9cd6f9d2ab162901552590b76438f35c0",
 	"0xbeb01aab5167402a5e89d057a714fabff94c3112",
 	"0xbfc14bc72590eb653af0873d3401fac602942ad3",
-	"0xC40dd2e85459e841021DD84F550645901aA337F9",
+	"0xc40dd2e85459e841021dd84f550645901aa337f9",
 	"0xc5a6d80bc7a2c3093a68e2ad05e8205dc0c0726b",
-	"0xCC14dB67F6E8d7CF0D9885223A3099A75D7Bdff2",
+	"0xcc14db67f6e8d7cf0d9885223a3099a75d7bdff2",
 	"0xcd0aa623f14cd369c1a2d3aacb96cf9157323795",
 	"0xcfb9fc585ed11bb8a6e79547920d825b6666bf55",
-	"0xdE1391114b3FcC3053c25127B399080F89CF6E71",
+	"0xde1391114b3fcc3053c25127b399080f89cf6e71",
 	"0x0795632dbe95bde7c896d48f794278661ef2054c",
-	"0x3c0b9c1690A94Fcc6994402d1ed8754439941835",
-	"0xaFCcA66cA8737FE647B7E94CB1a0891a72ab3AE5",
-	"0x4f89dF4406224693db4FD496A4066c8dE240CC53",
+	"0xafcca66ca8737fe647b7e94cb1a0891a72ab3ae5",
+	"0xf472067df9f1ef13da784f9181205506bc2cf01a",
+	"0x4f89df4406224693db4fd496a4066c8de240cc53",
 	"0x2fbaf345102ad37f2f4dbc1017089eed978b3c05",
-	"0x0624Aa196299088f7229dEFcee316dF5198C1255",
-	"0xB58dae4ebFCa3AeB21fF534991367f744b34c118",
+	"0x37d3745b398a4dcd2daf62a54d507d353079c72a",
 	"0xb7fcae88cab094a3372ce59b15be4c6e5f408762",
+	"0x01bf77d8adafd930b99c4fc01ed5eaf616bc6d93",
+	"0x3c0b9c1690a94fcc6994402d1ed8754439941835",
 	"0xd1444046e3987e44c8757a2400e9f8b7539f86b6",
-	"0x1cca19b823afa773b09708d94d2ee6ff96c60057",
-	"0x59F3c40a980b4155C8ED22DA76B4c8Ba72020291",
-	"0x01Bf77d8adaFd930b99C4fc01Ed5eAF616Bc6D93",
-	"0x7CD7D564c2BfA56A745E89204C457E1205595348",
-	"0xf146f3bf137683c6d2fe266f6a2dc352a615eb0c",
 	"0x7d5b4d1da35bb09c950295d6064a0f167346279f",
-	"0xdeE9D3a767e08AFCffc909e55d853a0069c2f46F",
-	"0x93420FFA0D098a0f10E34cd4495500a1CAf35888",
-	"0xab51ad23d222fd0afb4e29f3244402af9aa3c420",
-	"0xfBfaC05bd50e868fBAa842363883b23d9045A24f",
+	"0xb58dae4ebfca3aeb21ff534991367f744b34c118",
+	"0x4744cda32be7b3e75b9334001da9ed21789d4c0d",
+	"0x59f3c40a980b4155c8ed22da76b4c8ba72020291",
+	"0xf146f3bf137683c6d2fe266f6a2dc352a615eb0c",
+	"0x1cca19b823afa773b09708d94d2ee6ff96c60057",
+	"0x7cd7d564c2bfa56a745e89204c457e1205595348",
+	"0xdee9d3a767e08afcffc909e55d853a0069c2f46f",
+	"0x0624aa196299088f7229defcee316df5198c1255",
 	"0xcf61e54644f634d22b751b586f1fbe5f8f562c60",
-	"0xc3e06A42655E1988476089773C6f0ACB697167E7",
-	"0x15c7281842A45465B4cbb8F89111d99e36e5bab8",
+	"0x93420ffa0d098a0f10e34cd4495500a1caf35888",
+	"0xfbfac05bd50e868fbaa842363883b23d9045a24f",
+	"0xab51ad23d222fd0afb4e29f3244402af9aa3c420",
+	"0xc3e06a42655e1988476089773c6f0acb697167e7",
+	"0x15c7281842a45465b4cbb8f89111d99e36e5bab8"
 ];
 let data_balance = [];
 const chainId = 80001 // Mumbai testnet
@@ -162,8 +166,10 @@ const GetAndShowBalance = async () => {
 
           console.log("Amount:::: "+da01_amount)
           console.log("Get Amount success");
-          $("#score").text("Score："+da01_amount+" DA01");
-          $("#pfp").attr("src","./assets/img/result/"+coinbase+".png");
+          $("#score").text("Ø"+da01_amount);
+          // $("#pfp").attr("src","./assets/img/result/"+coinbase+".png");
+          $("#before_connect").hide();
+          $("#after_connect").show();
 
           fetch('https://epic-slack-app.herokuapp.com/api/json/users/'+coinbase+'/real_name').then(function(response_name) {
               // 直接轉成JSON格式
@@ -174,7 +180,16 @@ const GetAndShowBalance = async () => {
               console.log(obj);
               console.log(obj['real_name']);
               slackId = obj['real_name'];
-              $("#slackId").text('@'+slackId);
+
+              const a = '@'+slackId+'\n'+(String(coinbase).substr(0,4)+'...'+String(coinbase).substr(38,4));
+              
+              const a1 = a.replace(/\\u([0-9A-F]{4})/ig, (_, g) => String.fromCharCode(`0x${g}`));
+
+              a2 = a1.replaceAll('\n','<br>');
+      
+              // $("#slackId").text('@'+slackId+'<br>'+'123');
+              $("#slackId").empty();
+							$("#slackId").append(a2);
 
               fetch('https://epic-slack-app.herokuapp.com/api/json/users/'+coinbase+'/epic002').then(function(response_epic) {
                   // 直接轉成JSON格式
@@ -197,8 +212,8 @@ const GetAndShowBalance = async () => {
                   }
 
                   for(var k=0 ; k<obj2['epic002'].length ; k++ ){
-                  	epic.push('\n\n\n第'+(k+1)+'則史詩\n');
-                  	epic.push(String(epic_notSort[k]));
+                  	// epic.push('\n第'+(k+1)+'則史詩\n');
+                  	epic.push('第'+(k+1)+'則史詩\n'+String(epic_notSort[k])+'\n');
 
                   }
                  	console.log('EP::::'+String(epic));
@@ -303,7 +318,10 @@ const connectWalletGetAddress = async () => {
 	    console.log(account);
 	    coinbase = String(accounts[0]); 
 	    $("#C1").text(String(coinbase).substr(0,4)+'...'+String(coinbase).substr(38,4));
-	    $("#CWA").css('background','#16B8CE');
+	    // $("#CWA").css('background','#16B8CE');
+	    $("#connect_text").hide();
+	    $("#connect_button_2").hide();
+	    $("#shoutouts").show();
 
 	    console.log("ethereum.networkVersion0000000::::: "+ethereum.networkVersion);
 	    
@@ -321,8 +339,10 @@ const connectWalletGetAddress = async () => {
 	    	    console.log(account);
 	    	    coinbase = String(accounts[0]); 
 	    	    $("#C1").text(String(coinbase).substr(0,4)+'...'+String(coinbase).substr(38,4));
-	    	    $("#CWA").css('background','#16B8CE');
-	    	    
+	    	    // $("#CWA").css('background','#16B8CE');
+	    	    $("#connect_text").hide();
+	    	    $("#connect_button_2").hide();
+	    	    $("#shoutouts").show();
 
 	    	    GetAndShowBalance();
 	    	    
@@ -339,22 +359,22 @@ const connectWalletGetAddress = async () => {
 }
 
 
-function htmlForTextWithEmbeddedNewlines(text) {
-    var htmls = [];
-    var lines = text.split(/\n/);
-    // The temporary <div/> is to perform HTML entity encoding reliably.
-    //
-    // document.createElement() is *much* faster than jQuery('<div></div>')
-    // http://stackoverflow.com/questions/268490/
-    //
-    // You don't need jQuery but then you need to struggle with browser
-    // differences in innerText/textContent yourself
-    var tmpDiv = jQuery(document.createElement('html'));
-    for (var i = 0 ; i < lines.length ; i++) {
-        htmls.push(tmpDiv.text(lines[i]).html());
-    }
-    return htmls.join("<br>");
-}
+// function htmlForTextWithEmbeddedNewlines(text) {
+//     var htmls = [];
+//     var lines = text.split(/\n/);
+//     // The temporary <div/> is to perform HTML entity encoding reliably.
+//     //
+//     // document.createElement() is *much* faster than jQuery('<div></div>')
+//     // http://stackoverflow.com/questions/268490/
+//     //
+//     // You don't need jQuery but then you need to struggle with browser
+//     // differences in innerText/textContent yourself
+//     var tmpDiv = jQuery(document.createElement('html'));
+//     for (var i = 0 ; i < lines.length ; i++) {
+//         htmls.push(tmpDiv.text(lines[i]).html());
+//     }
+//     return htmls.join("<br>");
+// }
 
 //get slack name and epic
 
@@ -478,7 +498,7 @@ function htmlForTextWithEmbeddedNewlines(text) {
 // 		}else{
 // 			console.log('data_pfp[i][1] > 60:::'+data_pfp[i][0].toLowerCase());
 // 			console.log('data_pfp[i][1] > 60:::'+data_pfp[i][1]);
-// 			fs.copyFile('./assets/img/material/Level3/'+data_pfp[i][2]+'-'+data_pfp[i][3]+'-'+data_pfp[i][4]+'.png', './assets/img/result/'+data_pfp[i][0].toLowerCase()+'.png', (err) => {
+// 			fs.copyFile('./assets/img/material/Level4/'+data_pfp[i][2]+'-'+data_pfp[i][3]+'-'+data_pfp[i][4]+'-'+data_pfp[i][5]+'.png', './assets/img/result/'+data_pfp[i][0].toLowerCase()+'.png', (err) => {
 // 			  if (err) throw err;
 // 			  console.log('pfp was copied');
 // 			});
@@ -521,60 +541,48 @@ function htmlForTextWithEmbeddedNewlines(text) {
 
 // 	// let rawdata_address = fs.readFileSync('./mintlist.json');
 // 	let address = [
-		
-		
-		
-		
-		
-		
-// 		"0xbeb01aab5167402a5e89d057a714fabff94c3112",
-// 		"0xbfc14bc72590eb653af0873d3401fac602942ad3",
-// 		"0xC40dd2e85459e841021DD84F550645901aA337F9",
-// 		"0xc5a6d80bc7a2c3093a68e2ad05e8205dc0c0726b",//
-// 		"0xCC14dB67F6E8d7CF0D9885223A3099A75D7Bdff2",
-// 		"0xcd0aa623f14cd369c1a2d3aacb96cf9157323795",//
-// 		"0xcfb9fc585ed11bb8a6e79547920d825b6666bf55",//
-// 		"0xdE1391114b3FcC3053c25127B399080F89CF6E71"
+
 // 	];
+// 	console.log("address.length");
 // 	console.log(address.length);
 
 // 	// let rawdata_amounts = fs.readFileSync('./mintlist_i.json');
-// 	let amounts = [
-// 		"2",
-// 		"2",
-// 		"2",
-// 		"2",
-// 		"2",
-// 		"12",
-// 		"2",
-// 		"2",
-// 		"2",
-// 		"2",
-// 		"2",
-// 		"2",
-// 		"2",
-// 		"2"];
-// 	console.log(amounts[0]);
+// 	// let amounts = [
+// 	// 	"2",
+// 	// 	"2",
+// 	// 	"2",
+// 	// 	"2",
+// 	// 	"2",
+// 	// 	"12",
+// 	// 	"2",
+// 	// 	"2",
+// 	// 	"2",
+// 	// 	"2",
+// 	// 	"2",
+// 	// 	"2",
+// 	// 	"2",
+// 	// 	"2"];
+// 	// console.log(amounts[0]);
 
-// 	for(var t=0 ; t<amounts.length; t++){
+// 	for(var t=0 ; t<address.length; t++){
 
 // 		console.log(address[t]);
 
-// 		for (var i = 0 ; i < amounts[t] ; i++) {//student.length
-// 			console.log(i+1);
+// 		// for (var i = 0 ; i < amounts[t] ; i++) {//student.length
+// 			console.log(t+1);
 // 			mint(address[t]);
 		
 
 
 		
-// 		}
+// 		// }
 // 	}
 	
 // }
 
 
 
-// mint("0xfBfaC05bd50e868fBAa842363883b23d9045A24f");
+
 
 
                   
